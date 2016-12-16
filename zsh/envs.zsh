4d7edb29e875c831799643ae77b5b21fbf95d0f7
Suvinay Subramanian <suvinay@csail.mit.edu>
2016-12-16 16:18:15 -0500

    [zsh] Some environment variables.

---
+++ zsh/envs.zsh
@@ -0,0 +1,5 @@
+# Paths to folders
+export ORDSIM=$HOME/Myspace/projects/ordspecsim/
+export ORDBENCH=$HOME/Myspace/projects/ordspec-benchmarks/
+export ORDSCRIPTS=$HOME/Myspace/projects/ordspec-scripts/
+export OSSTEMP=$HOME/temp/ordspecsim/
