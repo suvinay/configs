b469e5e1c78e66a5c309d3f6b0a082dd7194130b
Suvinay Subramanian <suvinay@csail.mit.edu>
2016-12-16 18:17:09 -0500

    [zsh] Update PATH variable for Macports

--- zsh/envs.zsh
+++ zsh/envs.zsh
@@ -3,3 +3,9 @@ export ORDSIM=$HOME/Myspace/projects/ordspecsim/
 export ORDBENCH=$HOME/Myspace/projects/ordspec-benchmarks/
 export ORDSCRIPTS=$HOME/Myspace/projects/ordspec-scripts/
 export OSSTEMP=$HOME/temp/ordspecsim/
+
+# For Macports
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
+
+export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
