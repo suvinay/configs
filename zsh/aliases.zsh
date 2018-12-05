996b7838642b64ae1485f8bc3e4d45ef102429b0
Suvinay Subramanian <suvinays89@gmail.com>
2018-12-05 10:44:18 -0800

    [zsh] Update aliases, simple zsh scripts.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -44,6 +44,16 @@ rmclient() { g4 citc -d $1; }
 compctl -W /google/src/cloud/${USER}/ -/ rmclient
 
 # G4/P4
-alias gdiff='P4DIFF=colordiff G4MULTIDIFF=0 g4 diff'
+# It's better to just use cs/ and the web interface for most tasks. These are meant to be a quick-and-dirty replacement for use in the terminal.
+# $ gdiff               : Show all _pending_ changes in current client along with their diffs (i.e. changes to files).
+# $ gdiff /path/to/file : Show _pending_ changes to /path/to/file in current client. If file has no pending changes, then empty output.
+# $ glocal              : Show all local (i.e. in this depot) CLs (submitted, as well as committed / pending). Pending CLs are highlighted in red.
+# $ glog                : <No arguments> => Show log for current directory [[ all CLs originating from any client ]]
+# $ glog /path/to/file  : Show log for /path/to/file
+#                         Unlike git log, what glog (without any argument) does is recursively call glog for each file in the directory. 
+#                         This is painful, because you can have the same CL show up multiple times (if a CL touched multiple files).
+# $ gshow <clnumber>    : Show changes made in specified clnumber
+alias gdiff='P4DIFF=colordiff G4MULTIDIFF=0 g4 diff 2> /dev/null'
 alias glocal='localchanges | sed -e "s/\(Change [[:digit:]]\+\)/$(tput setaf 3)\1$(tput sgr0)/" | sed -e "s/\(Change [[:digit:]]\+\)\(.*\)\(pending\)/$(tput setaf 3)\1$(tput sgr0)\2$(tput setaf 1)\3$(tput sgr0)/"'
 alias glog='$HOME/bin/glog '
+alias gshow='p4 change -o '
